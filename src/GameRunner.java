8c8d371ceb16816e52f14a5691c92c8e3b12fc9a
KyleY9 <KyleY9@nycstudents.net>
2023-11-03 12:33:40 -0400

    A super tiny edit.

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -6,7 +6,7 @@ public class GameRunner {
         // initial printing + receiving user input
         System.out.println("Welcome to the Game.\n\nIn front of you lay three doors, labeled 1, 2, and 3.");
         System.out.println("Two of these doors contain a bomb, and will explode upon opening. The other, however, will contain a brand new lamborghini!");
-        System.out.print("Now, which door will you choose? (1, 2, or 3) ");
+        System.out.print("Now, which door will you choose? 1, 2, or 3: ");
         int playerChoice = scan.nextInt();
 
         MontyHallGame instance = new MontyHallGame(playerChoice);
